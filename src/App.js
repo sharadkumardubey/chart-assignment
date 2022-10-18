64cfab152f440d7bfb4b5cd2f53bee3adf66bc4c
Sharad <35634504+sharadkumardubey@users.noreply.github.com>
2022-10-18 08:41:27 +0530

    added changes to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Legend, Line, LineChart, XAxis, YAxis } from 'recharts';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    fetch("https://mocki.io/v1/5cc3f57f-7050-4173-b69d-90db40a5297d")
+    .then((resp)=>resp.json())
+    .then((resp)=>setData(resp.data))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <XAxis dataKey="x"/>
+          <YAxis domain={[0, 'dataMax+200']} allowDataOverflow={true} />
+          <Legend />
+          <Line type="monotone" dataKey="value" fill='#FFF00000' />
+        </LineChart>
     </div>
   );
 }
